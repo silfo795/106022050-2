60e7b28996a85a5f29847e0032cfc3135d97f92f
silfo795 <a143050@gmail.com>
2017-09-18 15:13:18 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("學號：960101761\n系級：行動商務與多媒體應用學系一年級\n姓名：蔡咕咕\n時間：2016/03/04 \n地點：\"I319\"\n");
+
+    }
+}
